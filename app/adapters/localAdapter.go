adfe108ae244e7094c6d4dccc5238e9485a9487d
fajar siagian <fajar7xx@gmail.com>
2024-04-18 11:44:30 +0700

    change return if user inactive (main)

--- app/adapters/localAdapter.go
+++ app/adapters/localAdapter.go
@@ -204,7 +204,7 @@ func (adp *LocalAdapter) getProfile(ctx *gin.Context) (map[string]any, error) {
 	query.Take(&transformer)
 
 	if transformer["id"] == nil {
-		return transformer, fmt.Errorf("user in not registered on inactive", authField)
+		return transformer, fmt.Errorf("user is not registered on inactive", authField)
 	}
 
 	if err := dutils.CheckPassword(input["password"].(string), transformer["password"].(string)); err != nil {
